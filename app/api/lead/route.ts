a86658fcaeed20a262d6692c5002a39370e750c7
rudrajain95 <rudrajain95@gmail.com>
2026-03-04 02:31:10 +0530

    Final correct lead API

---
+++ app/api/lead/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const lead = await prisma.Lead.create({
+      data: {
+        name: body.name,
+        mobile: body.mobile,
+        address: body.address,
+        quantity: body.quantity,
+        time: body.time,
+      },
+    });
+
+    return Response.json({ success: true, lead });
+  } catch (error) {
+    console.error("API ERROR:", error);
+    return Response.json({ success: false });
+  }
+}
