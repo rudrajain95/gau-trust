0c557ba4f627123ff4c508814beceb8a89e88391
rudrajain95 <rudrajain95@gmail.com>
2026-03-04 20:33:22 +0530

    Fix layout.tsx syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "Gau Trust",
-  description: "Premium Dairy Marketplace"
+  description: "Premium Dairy Marketplace",
 };
 
 export default function RootLayout({
@@ -9,14 +9,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-   <html lang="en">
-
-<head>
-<meta name="google-site-verification" content="dpwXPKxiCgb2PmMek9EoNGXKKzKul6aqYr-sMO--iJI" />
-</head>
-
-<body style={{ margin: 0, fontFamily: "sans-serif" }}>
-{children}
-</body>
-
-</html>
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>{children}</body>
+    </html>
+  );
+}
