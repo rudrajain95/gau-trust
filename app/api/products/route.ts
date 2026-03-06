ddb68b779d124b4e676a58ef8a157a26f26c597c
rudrajain95 <rudrajain95@gmail.com>
2026-03-06 11:04:43 +0530

    create product api

---
+++ app/api/products/route.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const products = await prisma.product.findMany({
+    where: { active: true }
+  });
+
+  return NextResponse.json(products);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const product = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+      unit: body.unit
+    }
+  });
+
+  return NextResponse.json(product);
+}
