d93af6330544992e26ef1b3a510832250b250cad
rudrajain95 <rudrajain95@gmail.com>
2026-03-08 15:45:42 +0530

    Create route.ts

---
+++ app/api/orders/route.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(){
+
+const orders = await prisma.order.findMany({
+
+where:{
+status:"Pending"
+},
+
+orderBy:{
+createdAt:"desc"
+}
+
+});
+
+return NextResponse.json(orders);
+
+}
