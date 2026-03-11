ec19fd156aca88f4b7a020b459aa8fd4a7c69101
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 01:28:29 +0530

    Create page.tsx

---
+++ app/admin/settings/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function AdminSettings(){
+
+return(
+
+<div style={{
+padding:40,
+fontFamily:"Arial"
+}}>
+
+<h1>Admin Settings</h1>
+
+<p>Admin configuration settings will appear here.</p>
+
+</div>
+
+)
+
+}
