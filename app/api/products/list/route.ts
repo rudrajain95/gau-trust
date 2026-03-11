a5e0f2f5792e8d15f3d769d6e75214c757aefbe4
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 02:40:00 +0530

    Create route.ts

---
+++ app/api/products/list/route.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(){
+
+const products = await prisma.product.findMany({
+where:{
+active:true
+},
+orderBy:{
+createdAt:"desc"
+}
+});
+
+return NextResponse.json(products);
+
+}
