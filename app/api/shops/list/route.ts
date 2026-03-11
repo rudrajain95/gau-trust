ff7a98644bb86d1503c6f9d9401dfd2e232e2bd4
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 02:18:48 +0530

    Update route.ts

--- app/api/shops/list/route.ts
+++ app/api/shops/list/route.ts
@@ -1,1 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
+
+export async function GET() {
+
+  try {
+
+    const shops = await prisma.shop.findMany({
+      orderBy: { createdAt: "desc" }
+    });
+
+    return NextResponse.json(shops);
+
+  } catch (error) {
+
+    return NextResponse.json({
+      success: false,
+      message: "Server error"
+    });
+
+  }
+
+}
