2868a60b6bd194ce3c37d3116dd2e05ccde3ce85
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 01:33:37 +0530

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -4,40 +4,79 @@ export default function AdminDashboard(){
 
 return(
 
-<div style={{padding:30,fontFamily:"Arial"}}><h1>Admin Control Panel</h1><div style={{
+<div style={{
+padding:40,
+fontFamily:"Arial"
+}}>
+
+<h1>Admin Dashboard</h1>
+
+<div style={{
 display:"grid",
 gridTemplateColumns:"1fr 1fr",
-gap:20,
-marginTop:30
-}}><a href="/admin/products">
-<div style={{padding:30,border:"1px solid #ddd",borderRadius:10,textAlign:"center"}}>
+gap:30,
+marginTop:40
+}}>
+
+<a href="/admin/products">
+<div style={{
+border:"1px solid #ddd",
+padding:30,
+borderRadius:10,
+textAlign:"center"
+}}>
 <h3>Products</h3>
 </div>
-</a><a href="/admin/orders">
-<div style={{padding:30,border:"1px solid #ddd",borderRadius:10,textAlign:"center"}}>
+</a>
+
+<a href="/admin/orders">
+<div style={{
+border:"1px solid #ddd",
+padding:30,
+borderRadius:10,
+textAlign:"center"
+}}>
 <h3>Orders</h3>
 </div>
-</a><a href="/admin/delivery">
-<div style={{padding:30,border:"1px solid #ddd",borderRadius:10,textAlign:"center"}}>
+</a>
+
+<a href="/admin/delivery">
+<div style={{
+border:"1px solid #ddd",
+padding:30,
+borderRadius:10,
+textAlign:"center"
+}}>
 <h3>Delivery</h3>
 </div>
-</a><a href="/admin/leads">
-<div style={{padding:30,border:"1px solid #ddd",borderRadius:10,textAlign:"center"}}>
+</a>
+
+<a href="/admin/customers">
+<div style={{
+border:"1px solid #ddd",
+padding:30,
+borderRadius:10,
+textAlign:"center"
+}}>
 <h3>Customers</h3>
 </div>
-</a><div
-onClick={()=>{
-window.location.href="/";
-}}
-style={{
-padding:30,
+</a>
+
+<a href="/admin/settings">
+<div style={{
 border:"1px solid #ddd",
+padding:30,
 borderRadius:10,
-textAlign:"center",
-cursor:"pointer"
-}}
->
-<h3>Logout</h3>
-</div></div></div>)
+textAlign:"center"
+}}>
+<h3>Settings</h3>
+</div>
+</a>
+
+</div>
+
+</div>
+
+)
 
 }
