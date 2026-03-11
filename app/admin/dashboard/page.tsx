c35c96e6c943fda8998ab7564dc3db856d056729
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 01:46:38 +0530

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function AdminDashboard(){
+
+useEffect(()=>{
+
+const admin = localStorage.getItem("adminLogin");
+
+if(!admin){
+window.location.href="/admin/login";
+}
+
+},[]);
 
 return(
 
@@ -72,6 +84,23 @@ textAlign:"center"
 <h3>Settings</h3>
 </div>
 </a>
+
+<div
+onClick={()=>{
+localStorage.removeItem("adminLogin");
+document.cookie = "admin_auth=; path=/; max-age=0";
+window.location.href="/admin/login";
+}}
+style={{
+border:"1px solid #ddd",
+padding:30,
+borderRadius:10,
+textAlign:"center",
+cursor:"pointer"
+}}
+>
+<h3>Logout</h3>
+</div>
 
 </div>
 
