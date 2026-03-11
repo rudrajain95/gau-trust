50e56cc11d828e6f6ace7c8ae55ab7776a5e3b06
rudrajain95 <rudrajain95@gmail.com>
2026-03-12 01:27:40 +0530

    Create page.tsx

---
+++ app/admin/customers/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function AdminCustomers(){
+
+return(
+
+<div style={{
+padding:40,
+fontFamily:"Arial"
+}}>
+
+<h1>Customers</h1>
+
+<p>All registered customers will appear here.</p>
+
+</div>
+
+)
+
+}
