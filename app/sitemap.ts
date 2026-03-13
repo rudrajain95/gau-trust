22f5a9269d6a42093036705041fc214f31257c4f
rudrajain95 <rudrajain95@gmail.com>
2026-03-14 02:30:58 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.gautrustmilk.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.gautrustmilk.in/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.gautrustmilk.in/signup",
+      lastModified: new Date(),
+    },
+  ];
+}
