c167863eda01f4d90ea64fd1d5da9ee415b15439
rudrajain95 <rudrajain95@gmail.com>
2026-03-14 12:19:10 +0530

    Update route.ts

--- app/api/shop/orders/route.ts
+++ app/api/shop/orders/route.ts
@@ -3,28 +3,25 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(req:Request){
+export async function GET(req: Request){
 
-const {searchParams}=new URL(req.url);
+  const { searchParams } = new URL(req.url);
 
-const shopId = searchParams.get("shopId");
+  const shopId = searchParams.get("shopId");
 
-if(!shopId){
-return NextResponse.json([]);
-}
+  if(!shopId){
+    return NextResponse.json([]);
+  }
 
-const orders = await prisma.order.findMany({
+  const orders = await prisma.order.findMany({
+    where:{
+      shopId
+    },
+    orderBy:{
+      createdAt:"desc"
+    }
+  });
 
-where:{
-shopId
-},
-
-orderBy:{
-createdAt:"desc"
-}
-
-});
-
-return NextResponse.json(orders);
+  return NextResponse.json(orders);
 
 }
